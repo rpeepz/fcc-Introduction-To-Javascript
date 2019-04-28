33b5c989367cd694823e949b18f45bd79763ce2e
Christian Bagdon <cbagdon119@gmail.com>
2019-04-28 11:02:19 -0700

    Initial Commit

---
+++ Subjects/Appending Variables to Strings/appending.js
@@ -0,0 +1,11 @@
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "FuN tHeY sAiD";
+var myStr = "Learning to code is "
+
+myStr += someAdjective;
