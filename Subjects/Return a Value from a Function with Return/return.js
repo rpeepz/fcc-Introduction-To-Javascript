4017deca38ed3c999eae6e6f7d052514f81a6c6a
Christian Bagdon <cbagdon119@gmail.com>
2019-04-29 15:49:08 -0700

    Initial Commit

---
+++ Subjects/Return a Value from a Function with Return/return.js
@@ -0,0 +1,11 @@
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(number)
+{
+  return (number * 5);
+}
+console.log(minusSeven(10));
