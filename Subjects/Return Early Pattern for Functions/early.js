9644c2bc6372afd821d3bccf534385c9a35a8335
Christian Bagdon <cbagdon119@gmail.com>
2019-05-09 20:54:53 -0700

    Initial Commit

---
+++ Subjects/Return Early Pattern for Functions/early.js
@@ -0,0 +1,11 @@
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < b)
+    return (undefined);
+  // Only change code above this line
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Change values below to test your code
+abTest(2,2);
