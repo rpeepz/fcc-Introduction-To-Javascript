e2def62199f1410b928085171f50c3ffc2426327
Christian Bagdon <cbagdon119@gmail.com>
2019-05-10 17:04:15 -0700

    Initial Commit

---
+++ Subjects/Manipulating Complex Objects/complex.js
@@ -0,0 +1,23 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Blackbear",
+    "title": "Drug Dealer",
+    "release_year": 2019,
+	"formats": [
+		"CD",
+		"Vinyl",
+		"Digital Streaming Services"
+	 ],
+  }
+];
