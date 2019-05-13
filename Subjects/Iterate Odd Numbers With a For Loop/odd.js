94256200db3fc5f744c7dbc6035cc2e742c60ddc
Christian Bagdon <cbagdon119@gmail.com>
2019-05-13 12:39:46 -0700

    Initial Commit

---
+++ Subjects/Iterate Odd Numbers With a For Loop/odd.js
@@ -0,0 +1,13 @@
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (let i = 1; i <= 9; i += 2)
+  myArray.push(i);
